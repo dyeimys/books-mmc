07469976d46284548bad4db0b184ac4066842bbb
Milena <michailoffcomarella@gmail.com>
2023-03-12 15:38:25 -0300

    Adição de algumas propriedades dos livros.

--- src/main/kotlin/com/mmc/livros/dto/BookDto.kt
+++ src/main/kotlin/com/mmc/livros/dto/BookDto.kt
@@ -3,6 +3,13 @@ package com.mmc.livros.dto
 data class BookDto(
         var id: Long? = null,
         var title: String,
-        var author: String
+        var author: String,
+        var genre: String,
+        var numPages: Int,
+        var synopsis: String,
+        var publishingCompany: String,
+        var publishingDate: Int,
+        var numBooks: Int,
+        var isbn: String
 ) {
 }
--- src/main/kotlin/com/mmc/livros/entity/BookEntity.kt
+++ src/main/kotlin/com/mmc/livros/entity/BookEntity.kt
@@ -10,6 +10,14 @@ class BookEntity(
         @GeneratedValue
         var id: Long? = null,
         var title: String,
+        var author: String,
+        var genre: String,
+        var numPages: Int,
+        var synopsis: String,
+        var publishingCompany: String,
+        var publishingDate: Int,
+        var numBooks: Int,
+        var isbn: String
 ) {
 
 }
