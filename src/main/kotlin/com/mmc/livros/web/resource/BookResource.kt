a14168ea90607d06970ffc6ee920d9605b3dd99e
Milena <michailoffcomarella@gmail.com>
2023-03-14 20:03:44 -0300

    Adição do mapper toDTO

--- src/main/kotlin/com/mmc/livros/web/resource/BookResource.kt
+++ src/main/kotlin/com/mmc/livros/web/resource/BookResource.kt
@@ -2,6 +2,7 @@ package com.mmc.livros.web.resource
 
 import com.mmc.livros.dto.BookDto
 import com.mmc.livros.service.BookService
+import com.mmc.livros.web.mapper.BookMapper
 import com.mmc.livros.web.request.BookRequest
 import com.mmc.livros.web.response.BookResponse
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,20 +16,24 @@ import org.springframework.web.bind.annotation.RestController
 class BookResource(
         @Autowired
         var bookService: BookService,
+
+        @Autowired
+        var bookMapper: BookMapper,
 ) {
 
     @PostMapping
     fun createBook(@RequestBody bookRequest: BookRequest): BookResponse {
-        val bookDto = BookDto(title = bookRequest.title,
-                author = bookRequest.author,
-                genre = bookRequest.genre,
-                isbn = bookRequest.isbn,
-                numBooks = bookRequest.numBooks,
-                numPages = bookRequest.numPages,
-                synopsis = bookRequest.synopsis,
-                publishingCompany = bookRequest.publishingCompany,
-                publishingDate = bookRequest.publishingDate)
+        val bookDto = bookMapper.toDto(bookRequest)
         val createBook = bookService.createBook(bookDto)
-        return BookResponse(createBook.id, createBook.title)
+        return BookResponse(createBook.id,
+                createBook.title,
+                createBook.author,
+                createBook.genre,
+                createBook.numPages,
+                createBook.synopsis,
+                createBook.publishingCompany,
+                createBook.publishingDate,
+                createBook.numBooks,
+                createBook.isbn)
     }
 }
