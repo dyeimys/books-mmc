c9e5898af32760afa3c290782791e167225b3abc
Milena <michailoffcomarella@gmail.com>
2023-03-26 22:53:43 -0300

    Adição de beanValidation na Entity de livros e alteração para data class.

--- src/main/kotlin/com/mmc/livros/entity/BookEntity.kt
+++ src/main/kotlin/com/mmc/livros/entity/BookEntity.kt
@@ -3,21 +3,22 @@ package com.mmc.livros.entity
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @Entity(name = "book")
-class BookEntity(
+data class BookEntity(
         @Id
         @GeneratedValue
-        var id: Long? = null,
-        var title: String,
-        var author: String,
-        var genre: String,
-        var numPages: Int,
-        var synopsis: String,
-        var publishingCompany: String,
-        var publishingDate: Int,
-        var numBooks: Int,
-        var isbn: String
-) {
-
-}
+        @field:NotBlank var id: Long? = null, //não entendi muito bem esse "Long? = null", podes me explicar?
+        @field:Size(min = 1) var title: String,
+        @field:Size(min = 1) var author: String,
+        @field:NotBlank var genre: String,
+        @field:NotBlank var numPages: Int,
+        @field:NotBlank var synopsis: String,
+        @field:NotBlank var publishingCompany: String,
+        @field:NotBlank var publishingDate: Int,
+        @field:NotBlank var numBooks: Int,
+        @field:NotNull var isbn: String
+)
